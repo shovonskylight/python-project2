ad981c74f6effb853d8dc6b1ec1c04965e301e95
shovon mazumdar <shovonskylight@gmail.com>
2024-07-21 12:19:00 +0530

    assingment day 2

---
+++ simpleInterest.py
@@ -0,0 +1,5 @@
+p = float(input("Enter the Principal amount = "))
+r = float(input("Enter the rate of Interest = "))
+t = float(input("Enter the time = "))
+i = (p*r*t)/100
+print("simple interest = ", round(i,2))
---
+++ swapVar.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+a = a+b
+b = a-b
+a = a-b
+print("after swap the value of a = ",a,"and value of b = ",b)
