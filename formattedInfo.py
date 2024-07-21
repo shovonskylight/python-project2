5fdf12c9cd51070abd363886f29e41194ca22d94
shovon mazumdar <shovonskylight@gmail.com>
2024-07-21 12:30:46 +0530

    assingment day 2

---
+++ formattedInfo.py
@@ -0,0 +1,4 @@
+name = input("enter your name ")
+age = int(input("enter your age "))
+favColor = input("enter your favourite color ")
+print("Hi ",name, "your are ", age, "year's old and your favourite color is ", favColor)
